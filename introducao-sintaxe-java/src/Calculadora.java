d413107ebfb759fdba99183bc6bc57d002be5b7a
juliaazeved0 <juliacarolineazevedo@gmail.com>
2025-04-02 09:42:19 -0300

    exercicios

---
+++ introducao-sintaxe-java/src/Calculadora.java
@@ -0,0 +1,22 @@
+
+/** 
+* @author Julia
+* @version 1.0
+* @since 2025
+*/
+
+public class Calculadora {
+    /**
+     * @param numeroUm este é usado para somar dois numeros
+     * @param numeroDois este é usado como segundo parametr
+     * @return int o resultado da soma 
+     */
+    private int numeroUm;
+    private int  numeroDois;
+
+    public int soma(){
+        return numeroDois + numeroUm;
+    }
+
+
+}
